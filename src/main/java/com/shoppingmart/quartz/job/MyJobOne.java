8a12c47d856f845bce676e5c3e3cacdec7441c2e
purusottam16 <purusottam16@gmail.com>
2017-09-07 00:25:53 +0530

    Scheduler added for email notification

---
+++ src/main/java/com/shoppingmart/quartz/job/MyJobOne.java
@@ -0,0 +1,8 @@
+package com.shoppingmart.quartz.job;
+import org.springframework.stereotype.Service;
+@Service("jobone")
+public class MyJobOne {
+    protected void myTask() {
+    	System.out.println("This is my task");
+    }
+}
